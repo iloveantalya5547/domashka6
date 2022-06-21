79a3823920e005dedcadbb65aec1d21c15f7e443
TVOYA GERM1ONA <tishchenkomax818@gmail.com>
2022-06-22 00:25:52 +0300

    Initial commit

---
+++ GraphicFileReader/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Win32;
+using System.IO;
+using System.ComponentModel;
+using System.Windows.Media.Animation;
+using System.Threading;
+using System.Windows.Threading;
+
+
+namespace GraphicFileReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnshow.Click += Btnshow_Click;
+            btnclose.Click += Btnclose_Click;
+        }
+        private void mnuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+               "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+               "Portable Network Graphic (*.png)|*.png";
+         
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri fileUri = new Uri(openFileDialog.FileName);
+                img.Source = new BitmapImage(fileUri);
+                foreach (string filename in openFileDialog.FileNames)
+                    lbFiles.Items.Add(Path.GetFileName(filename));
+                BackgroundWorker worker = new BackgroundWorker();
+                worker.WorkerReportsProgress = true;
+                worker.DoWork += worker_DoWork;
+                worker.ProgressChanged += worker_ProgressChanged;
+
+                worker.RunWorkerAsync();
+            }
+          
+        }
+       
+        private void mnuExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Btnclose_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = Resources["CloseMenu"] as Storyboard;
+            sb.Begin(LeftMenu);
+        }
+        private void Btnshow_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = Resources["OpenMenu"] as Storyboard;
+            sb.Begin(LeftMenu);
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 100; i += 25)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbStatus.Value = e.ProgressPercentage;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Image read menu #
+
+## Main window ##
+![windw](./img/window.png)
+
+## File menu ##
+![file](./img/fileopen.png)
+
+## Loaded img ##
+![img](./img/loadedfile.png)
+
+## List of recent files ##
+![history](./img/history_list.png)
